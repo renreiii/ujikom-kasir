37c739518aea5cf7211fe47c9b229ef6bd97e12b
renreiii <rendi.raihan023@gmail.com>
2024-02-21 21:34:22 +0700

    [css] added minor fix for sidebar (highlight)

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -12,88 +12,88 @@
                 <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
         </div>
-        
-        <!-- /.search form -->
-        <!-- sidebar menu: : style can be found in sidebar.less -->
+
+        <!-- Sidebar menu -->
         <ul class="sidebar-menu" data-widget="tree">
-            <li>
+            <li class="{{ request()->routeIs('dashboard') ? 'active' : '' }}">
                 <a href="{{ route('dashboard') }}">
                     <i class="fa fa-dashboard"></i> <span>Dashboard</span>
                 </a>
             </li>
 
             @if (auth()->user()->level == 1)
-            <li class="header">MASTER</li>
-            <li>
-                <a href="{{ route('kategori.index') }}">
-                    <i class="fa fa-cube"></i> <span>Kategori</span>
-                </a>
-            </li>
-            <li>
-                <a href="{{ route('produk.index') }}">
-                    <i class="fa fa-cubes"></i> <span>Produk</span>
-                </a>
-            </li>
-            <li>
-                <a href="{{ route('member.index') }}">
-                    <i class="fa fa-id-card"></i> <span>Member</span>
-                </a>
-            </li>
-            <li>
-                <a href="{{ route('supplier.index') }}">
-                    <i class="fa fa-truck"></i> <span>Supplier</span>
-                </a>
-            </li>
-            <li class="header">TRANSAKSI</li>
-            
-            <li>
-                <a href="{{ route('pembelian.index') }}">
-                    <i class="fa fa-download"></i> <span>Pembelian</span>
-                </a>
-            </li>
-            <li>
-                <a href="{{ route('penjualan.index') }}">
-                    <i class="fa fa-upload"></i> <span>Penjualan</span>
-                </a>
-            </li>
-            <li>
-                <a href="{{ route('transaksi.index') }}">
-                    <i class="fa fa-cart-arrow-down"></i> <span>Transaksi Aktif</span>
-                </a>
-            </li>
-            <li>
-                <a href="{{ route('transaksi.baru') }}">
-                    <i class="fa fa-cart-arrow-down"></i> <span>Transaksi Baru</span>
-                </a>
-            </li>
-            <li class="header">REPORT</li>
-            <li>
-                <a href="{{ route('laporan.index') }}">
-                    <i class="fa fa-file-pdf-o"></i> <span>Laporan</span>
-                </a>
-            </li>
-            <li class="header">SYSTEM</li>
-            <li>
-                <a href="{{ route('user.index') }}">
-                    <i class="fa fa-users"></i> <span>User</span>
-                </a>
-            </li>
-            <li>
-                <a href="{{ route("setting.index") }}">
-                    <i class="fa fa-cogs"></i> <span>Pengaturan</span>
-                </a>
-            </li>
+                <li class="header">MASTER</li>
+                <li class="{{ request()->routeIs('kategori.*') ? 'active' : '' }}">
+                    <a href="{{ route('kategori.index') }}">
+                        <i class="fa fa-cube"></i> <span>Kategori</span>
+                    </a>
+                </li>
+                <li class="{{ request()->routeIs('produk.*') ? 'active' : '' }}">
+                    <a href="{{ route('produk.index') }}">
+                        <i class="fa fa-cubes"></i> <span>Produk</span>
+                    </a>
+                </li>
+                <li class="{{ request()->routeIs('member.*') ? 'active' : '' }}">
+                    <a href="{{ route('member.index') }}">
+                        <i class="fa fa-id-card"></i> <span>Member</span>
+                    </a>
+                </li>
+                <li class="{{ request()->routeIs('supplier.*') ? 'active' : '' }}">
+                    <a href="{{ route('supplier.index') }}">
+                        <i class="fa fa-truck"></i> <span>Supplier</span>
+                    </a>
+                </li>
+                <li class="header">TRANSAKSI</li>
+
+                <li class="{{ request()->routeIs('pembelian.*') ? 'active' : '' }}">
+                    <a href="{{ route('pembelian.index') }}">
+                        <i class="fa fa-download"></i> <span>Pembelian</span>
+                    </a>
+                </li>
+                <li class="{{ request()->routeIs('penjualan.*') ? 'active' : '' }}">
+                    <a href="{{ route('penjualan.index') }}">
+                        <i class="fa fa-upload"></i> <span>Penjualan</span>
+                    </a>
+                </li>
+                <li class="{{ request()->routeIs('transaksi.*') ? 'active' : '' }}">
+                    <a href="{{ route('transaksi.index') }}">
+                        <i class="fa fa-cart-arrow-down"></i> <span>Transaksi Aktif</span>
+                    </a>
+                </li>
+                <li class="{{ request()->routeIs('transaksi.baru') ? 'active' : '' }}">
+                    <a href="{{ route('transaksi.baru') }}">
+                        <i class="fa fa-cart-arrow-down"></i> <span>Transaksi Baru</span>
+                    </a>
+                </li>
+
+                <li class="header">REPORT</li>
+                <li class="{{ request()->routeIs('laporan.*') ? 'active' : '' }}">
+                    <a href="{{ route('laporan.index') }}">
+                        <i class="fa fa-file-pdf-o"></i> <span>Laporan</span>
+                    </a>
+                </li>
+                <li class="header">SYSTEM</li>
+                <li class="{{ request()->routeIs('user.*') ? 'active' : '' }}">
+                    <a href="{{ route('user.index') }}">
+                        <i class="fa fa-users"></i> <span>User</span>
+                    </a>
+                </li>
+                <li class="{{ request()->routeIs('setting.*') ? 'active' : '' }}">
+                    <a href="{{ route('setting.index') }}">
+                        <i class="fa fa-cogs"></i> <span>Pengaturan</span>
+                    </a>
+                </li>
             @else
-            <li>
-                <a href="{{ route('transaksi.index') }}">
-                    <i class="fa fa-cart-arrow-down"></i> <span>Transaksi Aktif</span>
-                </a>
-            </li>
-            <li>
-                <a href="{{ route('transaksi.baru') }}">
-                    <i class="fa fa-cart-arrow-down"></i> <span>Transaksi Baru</span>
-                </a>
-            </li>
+                <li class="{{ request()->routeIs('transaksi.*') ? 'active' : '' }}">
+                    <a href="{{ route('transaksi.index') }}">
+                        <i class="fa fa-cart-arrow-down"></i> <span>Transaksi Aktif</span>
+                    </a>
+                </li>
+                <li class="{{ request()->routeIs('transaksi.baru') ? 'active' : '' }}">
+                    <a href="{{ route('transaksi.baru') }}">
+                        <i class="fa fa-cart-arrow-down"></i> <span>Transaksi Baru</span>
+                    </a>
+                </li>
             @endif
         </ul>
     </section>
