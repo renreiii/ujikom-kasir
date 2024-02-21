9eaa4ca83918fc13c54eab506ec820c7b75ca99c
renreiii <rendi.raihan023@gmail.com>
2024-02-21 07:54:35 +0700

    seeder configuration

--- database/seeders/SettingTableSeeder.php
+++ database/seeders/SettingTableSeeder.php
@@ -16,11 +16,11 @@ class SettingTableSeeder extends Seeder
     {
         DB::table('setting')->insert([
             'id_setting' => 1,
-            'nama_perusahaan' => 'Toko Ku',
-            'alamat' => 'Jl. Kibandang Samaran Ds. Slangit',
-            'telepon' => '081234779987',
+            'nama_perusahaan' => 'Warung Gen Z',
+            'alamat' => 'Jl. Perintis, Ds Panulisan Barat',
+            'telepon' => '088213731813',
             'tipe_nota' => 1, // kecil
-            'diskon' => 5,
+            'diskon' => 0,
             'path_logo' => '/img/logo.png',
             'path_kartu_member' => '/img/member.png',
         ]);
