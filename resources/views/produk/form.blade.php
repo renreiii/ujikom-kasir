0df5e83776fbd17956b78427f05b00b4881e2c9b
renreiii <rendi.raihan023@gmail.com>
2024-03-06 00:17:31 +0700

    [fixed] cant add product cause form miss input attribute

--- resources/views/produk/form.blade.php
+++ resources/views/produk/form.blade.php
@@ -61,7 +61,7 @@
                     <div class="form-group row">
                         <label for="stok" class="col-lg-2 col-lg-offset-1 control-label">Stok</label>
                         <div class="col-lg-6">
-                            <input type="number" name="stok" id="stok" class="form-control" disabled value="0">
+                            <input type="number" name="stok" id="stok" class="form-control" readonly value="0">
                             <span class="help-block with-errors"></span>
                         </div>
                     </div>
